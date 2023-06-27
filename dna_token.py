da0b4d2cf7e8973cff122f38f5f30ae70dcaa2ac
István Szepesi-Nagy <szni98@gmail.com>
2023-06-27 10:17:30 +0200

    working kmerizer

--- dna_token.py
+++ dna_token.py
@@ -1,6 +1,13 @@
-def dna_token():
-    print("aasdasdasd")
+def dna_token(dna_string, kmer, step):
+    print(dna_string)
 
+    kmerized = ""
+    for i in range(0, len(dna_string)-kmer+1, step):
+        kmerized = kmerized + dna_string[i:i+kmer] + " "
+
+    return kmerized
 
 if __name__ == "__main__":
-    dna_token()
+    dna = "ACTGTCGTACAGTGACCAGTGGTAGT"
+    kmerized_dna = dna_token(dna, 5, 1)
+    print(kmerized_dna)
